5ac59156d110ba2e31dda3d2b3bb99a11321b22e
Tiny Labs Inc <elliot@tinylabs.io>
2024-04-30 12:01:05 -0600

    Rename to nanobind src bindings

--- bindings/mspfet.cpp
+++ bindings/mspfet.cpp
@@ -7,6 +7,6 @@ namespace nb = nanobind;
 using namespace nb::literals;
 
 NB_MODULE(mspfet, m) {
-    m.def("MSP430_Configure", &MSP430_Configure);
     m.def("MSP430_Initialize", &MSP430_Initialize);
+    m.def("MSP430_Close", &MSP430_Close);
 }
